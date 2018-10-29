ac329085b4c1fd76a6b497e6a5b087f923725fd1
davidkaggulire <dkaggulire@gmail.com>
2018-10-29 11:56:39 +0300

    Adds login and logout feature

--- login.py
+++ login.py
@@ -2,16 +2,20 @@
 import datetime
 from signup import User
 class Login(User):
-  """class shows methods for a user"""
+  """
+  class shows methods for a user
+  """
   def __init__(self, email, password, name, role)
     super.__init__(email, password, name, role)
 
   def login_user(self, email, password):
-    """method to login a user"""
+    """
+    method to login a user
+    """
     self.email = input('Enter email')
     self.password = input('Enter password')
     if self.email not in user_db and password not in user_db:
-      print ("Email " + self.email + " and "+ self.password+" doesnot exist. Please create register email")
+      print ("Email " + self.email + " and "+ self.password+" doesnot exist. Please register email")
 
     if self.email in user_db:
       self.logged_in = True
@@ -20,7 +24,9 @@ class Login(User):
       return "User logged in successfully"
 
   def logout_user(self):
-    """logs out the user"""
+    """
+    logs out the user
+    """
     if self.logged_in == True:
       self.logged_in == False
       print("User logged out successfully")
