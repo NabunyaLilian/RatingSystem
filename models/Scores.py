ff6bda868757719e9f40417c3c70eade77b70ac6
ruju256 <ezramahlon@gmail.com>
2018-10-29 12:14:58 +0300

    editing method rate_a_bootcamper

--- models/Scores.py
+++ models/Scores.py
@@ -3,21 +3,21 @@ import json
 class Scores:
 
     initial_score = 0
-    
-
+    epic_scores=[{}]
     def rate_a_bootcamper(self, excellence, passion, integrity, collaboration):
         global initial_score
-
-        excellence, passion, integrity, collaboration = 0, 0, 0, 0
+        global epic_scores
+        epic_scores=[{}]
         initial_score = excellence + passion + integrity + collaboration
 
         final_student_score = initial_score / 4
-
+        epic_scores.append([{
+            "excellence" :"excellence",
+            "passion": "passion",
+            "integrity":"integrity",
+            "collaboration":"collaboration"
+        }])
         print("This is the final score for the student", final_student_score)
 
-    def view_scores(self):
-
-        pass
-
-    def edit_scores(self):
-        pass
+    def view_scores(self):        
+       return epic_scores
